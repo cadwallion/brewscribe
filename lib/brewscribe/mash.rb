2c698a761324740cb346688142d41d2a66d6aa97
Andrew Nordman <cadwallion@gmail.com>
2012-04-07 08:43:45 -0500

    Adds Brewscribe::Mash key conversions

--- lib/brewscribe/mash.rb
+++ lib/brewscribe/mash.rb
@@ -6,13 +6,34 @@ module Brewscribe
       :tun_temp, :ph, :sparge_temp, :batch, :batch_pct, :batch_even, :batch_drain, 
       :mash_39, :tun_deadspace, :biab_vol, :biab, :notes, :steps, :equip_adjust, 
       :tun_vol, :tun_mass, :tun_hc
+
+    include Brewscribe::Conversion
+    
+    KEY_CONVERSION = {
+      last_modified: DATE_CONV,
+      grain_weight: FLOAT_CONV,
+      grain_temp: FLOAT_CONV,
+      boil_temp: FLOAT_CONV,
+      tun_temp: FLOAT_CONV,
+      ph: FLOAT_CONV,
+      sparge_temp: FLOAT_CONV,
+      batch: FLOAT_CONV,
+      batch_pct: PERCENT_CONV,
+      batch_even: BOOLEAN_CONV,
+      batch_drain: BOOLEAN_CONV,
+      mash_39: BOOLEAN_CONV,
+      tun_deadspace: FLOAT_CONV,
+      biab_vol: FLOAT_CONV,
+      biab: BOOLEAN_CONV,
+      equip_adjust: BOOLEAN_CONV,
+      tun_vol: FLOAT_CONV,
+      tun_mass: FLOAT_CONV,
+      tun_hc: FLOAT_CONV
+    }
     
     def self.from_data data 
       mash = new
-
-      data.keys.each do |key|
-        mash.send "#{key}=", data[key]
-      end
+      mash.data_to_properties data
       
       if mash.steps[:data][:mashstep].class == Hash
         steps = [mash.steps[:data][:mashstep]]
--- spec/mash_spec.rb
+++ spec/mash_spec.rb
@@ -14,5 +14,40 @@ describe Brewscribe::Mash do
       subject.steps.should be_a Array
       subject.steps[0].should be_a Brewscribe::Mash::Step
     end
+
+    it { subject.grain_weight.should be_a Float }
+    it { subject.grain_temp.should be_a Float }
+    it { subject.boil_temp.should be_a Float }
+    it { subject.tun_temp.should be_a Float }
+    it { subject.sparge_temp.should be_a Float }
+    it { subject.ph.should be_a Float }
+    it { subject.batch.should be_a Float }
+    it { subject.batch_pct.should be_a Float }
+    it { subject.tun_deadspace.should be_a Float }
+    it { subject.biab_vol.should be_a Float }
+    it { subject.tun_vol.should be_a Float }
+    it { subject.tun_mass.should be_a Float }
+    it { subject.tun_hc.should be_a Float }
+    it { subject.last_modified.should be_a Date }
+
+    it 'batch_even should be a boolean' do
+      [true, false].should include subject.batch_even
+    end
+
+    it 'batch_drain should be a boolean' do
+      [true, false].should include subject.batch_drain
+    end
+
+    it 'mash_39 should be a boolean' do
+      [true, false].should include subject.mash_39
+    end
+
+    it 'biab should be a boolean' do
+      [true, false].should include subject.biab
+    end
+
+    it 'equip_adjust should be a boolean' do
+      [true, false].should include subject.equip_adjust
+    end
   end
 end
