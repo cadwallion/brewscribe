5aa0381a7141c60eff63c70b7183b40bdd98e901
Andrew Nordman <cadwallion@gmail.com>
2013-03-09 21:25:35 -0600

    Adds be_boolean matcher, refactors Mash spec
    Mash spec drops use of the RECIPE_HASH[:mash] in favor of import_document.
    The specs have been improved and a new be_boolean matcher is in place for
    typecast validation.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,12 @@ require 'pry'
 def import_document
   Brewscribe.import File.open(File.dirname(__FILE__) + '/support/recipe.bsmx', 'r')
 end
+
+RSpec::Matchers.define :be_boolean do
+  match do |actual|
+    [true, false].include? actual
+  end
+  failure_message_for_should do |actual|
+    "expected #{actual} to be true or false"
+  end
+end
