0c781db416eadde3c4799cad37c3e8346aad3b96
Andrew Nordman <cadwallion@gmail.com>
2013-03-09 21:04:09 -0600

    Refactors Style spec to use recipe.bsmx
    Instead of using the RECIPE_HASH[:style], Style now imports the recipe.bsmx found in spec/support/.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,7 @@ $: << '../lib'
 require 'brewscribe'
 require './spec/support/recipe_hash'
 require 'pry'
+
+def import_document
+  Brewscribe.import File.open(File.dirname(__FILE__) + '/support/recipe.bsmx', 'r')
+end
